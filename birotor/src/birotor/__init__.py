5efafab3cfd8d097b25b9d860fc0f92e1c1915ae
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2026-06-12 14:11:12 +0200

    split into two modules

---
+++ birotor/src/birotor/__init__.py
@@ -0,0 +1,4 @@
+from . import dynamics
+from . import simulation
+
+__all__ = ["dynamics", "simulation"]
---
+++ controllers/src/controllers/__init__.py
@@ -0,0 +1,4 @@
+from . import infinite_horizon_regulators
+
+__all__ = ["infinite_horizon_regulators"]
+
